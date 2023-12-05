51a11ceb11b49623213c2e0f94f47845782f6eca
Chigozie Nri <chigozie@replicate.com>
2023-12-05 21:31:30 +0000

    Increase frame name zero padding

--- motionextractor.sh
+++ motionextractor.sh
@@ -9,6 +9,6 @@ input_video=$1
 frames_offset=${2:-2}
 target_video=${3:-output.mp4}
 temp_dir=$(mktemp -d)
-ffmpeg -i $input_video -vcodec png $temp_dir/input_%03d.png
-ffmpeg -i $input_video -vf "negate,format=rgba,colorchannelmixer=aa=0.5" -vcodec png $temp_dir/inverted_halfopacity_%03d.png
-ffmpeg -i $temp_dir/input_%03d.png -start_number $frames_offset -i $temp_dir/inverted_halfopacity_%03d.png -i $input_video -filter_complex "[0:v][1:v] overlay=0:0" -c:v libx264 -preset ultrafast -c:a copy $target_video
+ffmpeg -i $input_video -vcodec png $temp_dir/input_%06d.png
+ffmpeg -i $input_video -vf "negate,format=rgba,colorchannelmixer=aa=0.5" -vcodec png $temp_dir/inverted_halfopacity_%06d.png
+ffmpeg -i $temp_dir/input_%06d.png -start_number $frames_offset -i $temp_dir/inverted_halfopacity_%06d.png -i $input_video -filter_complex "[0:v][1:v] overlay=0:0" -c:v libx264 -preset ultrafast -c:a copy $target_video
