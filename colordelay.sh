99d04822de9bd5d9e5464d1f00517bb77aa90873
Chigozie Nri <chigozie@replicate.com>
2023-12-05 20:53:25 +0000

    color_delay

---
+++ colordelay.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Usage: ./colordelay.sh input.mp4 2 output.mp4
+# Delays the red, green and blue channels of a video by a specified number of frames
+# The first argument is the path to the video to extract motion from
+# The second, third and fourth arguments are the number of frames to delay the red, green and blue channels respectively (optional, defaults 0, 2, 4)
+# The fifth argument is the output video (optional, default output.mp4)
+
+input_video=$1
+red_frames_offset=${2:-2}
+green_frames_offset=${3:-2}
+blue_frames_offset=${4:-2}
+target_video=${5:-output.mp4}
+
+temp_dir=$(mktemp -d)
+# split RGB channels into 3 separate grayscale videos
+ffmpeg -i $input_video -filter_complex "[0:v]format=rgb24,split=3[r][g][b];[r]extractplanes=r[red];[g]extractplanes=g[green];[b]extractplanes=b[blue]" -map "[red]" $temp_dir/red.mp4 -map "[green]" $temp_dir/green.mp4 -map "[blue]" $temp_dir/blue.mp4
+
+ffmpeg -i $temp_dir/red.mp4 -vf trim=start_frame=$red_frames_offset,setpts=PTS-STARTPTS $temp_dir/red_shifted.mp4
+ffmpeg -i $temp_dir/green.mp4 -vf trim=start_frame=$green_frames_offset,setpts=PTS-STARTPTS $temp_dir/green_shifted.mp4
+ffmpeg -i $temp_dir/blue.mp4 -vf trim=start_frame=$blue_frames_offset,setpts=PTS-STARTPTS $temp_dir/blue_shifted.mp4
+
+# merge the shifted videos into GBRP format
+ffmpeg -i $temp_dir/green_shifted.mp4 -i $temp_dir/blue_shifted.mp4 -i $temp_dir/red_shifted.mp4 -filter_complex "[0:v][1:v][2:v]mergeplanes=0x001020:gbrp" $temp_dir/gbrp_video.mp4
+
+# convert the GBRP video to RGB format
+ffmpeg -i $temp_dir/gbrp_video.mp4 -i $input_video -vf "format=rgb24" -c:v libx264 -preset ultrafast -c:a copy $target_video
--- predict.py
+++ predict.py
@@ -21,6 +21,7 @@ class Predictor(BasePredictor):
             description="Frames to offset video by for motion extraction. Don't see any motion? Try increasing the number of frames.",
             default=2,
         ),
+        color_delay: str = Input(description="Number of frames to delay red, green and blue channels, respectively. Artistic option.", default="0,0,0"),
     ) -> Path:
         """Run a single prediction on the model"""
         # Remove for safety
@@ -28,11 +29,16 @@ class Predictor(BasePredictor):
         in_path = os.path.join(temp_dir, os.path.basename(input_video))
         extension = os.path.splitext(input_video)[1]
         if extension == "":
-            extension = "mp4"
+            extension = ".mp4"
         out_filename = "output" + extension
         out_path = os.path.join(temp_dir, out_filename)
         shutil.copyfile(input_video, in_path)
         subprocess.run(["./motionextractor.sh", in_path, str(frames_offset), out_path])
+
+        if color_delay != "0,0,0":
+            tmp_video_path = os.path.join(temp_dir, "tmp" + extension)
+            subprocess.run(["./colordelay.sh", out_path, *[n.strip() for n in color_delay.split(',')], tmp_video_path])
+            shutil.move(tmp_video_path, out_path)
         # Remove for safety
         if os.path.exists(out_filename):
             os.remove(out_filename)
